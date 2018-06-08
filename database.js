c2118beaea4e878f06ec640ba59c8821c4d7b047
Josh Coffee <joshualcoffee@gmail.com>
2018-06-08 11:55:26 -0500

    update kubernetes configs

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb://${process.env.MONGO_SERVICE_HOST}:${process.env.MONGO_SERVICE_PORT}/workforce_mock_location`).then(
+mongoose.connect(`mongodb://${process.env.WORKFORCE_FAKE_SERVICE_MONGO_SERVICE_SERVICE_HOST}:${process.env.WORKFORCE_FAKE_SERVICE_MONGO_SERVICE_SERVICE_PORT}/workforce_mock_location`).then(
     () => {console.log('connected') },
     err => { console.log('error' + " " + err )
 });
